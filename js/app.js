390d3efa47c805b7c62bc67df5f86df5b74fa7dd
mariasolegallina <gallinamariasole@gmail.com>
2023-10-04 16:12:59 +0200

    update - else sconti

--- js/app.js
+++ js/app.js
@@ -10,16 +10,25 @@ console.log(eta);
 
 // - calcolare il prezzo del biglietto:
 //     - moltiplicare il costo chilometrico per il numero di km da percorrere
-let costoChilometrico;
-costoChilometrico = kmDaPercorrere * 0.21;
-console.log(costoChilometrico);
+let costoBiglietto;
+costoBiglietto = kmDaPercorrere * 0.21;
+console.log(costoBiglietto);
 
 //     - appplicare uno sconto:
 //         - SE l'utente ha meno di 18 anni
 if (eta < 18) {
-    console.log('è minorenne')
-
-} else 
+ scontoGiovani = costoBiglietto * 0.2;
+ console.log(scontoGiovani);
+ costoGiovani = costoBiglietto - scontoGiovani;
+ console.log(costoGiovani);
+} else if (eta > 60) {
+ scontoAnziani = costoBiglietto * 0.4;
+ console.log(scontoAnziani);
+ costoAnziani = costoBiglietto - scontoAnziani;
+ console.log(costoAnziani);
+} else {
+ console.log(costoBiglietto);
+}
 //             - sottrarre il 20% al costo del biglietto
 //         - ALTRIMENTI SE l'utente ha più di 60 anni
 //             - sottrarre il 40% al costo del biglietto
